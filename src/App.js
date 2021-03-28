1d8a988fb48a43efd3fe897a11b246e850adc852
maciej <maciej0905@gmail.com>
2021-03-28 21:29:02 +0200

    part 1 exercises 1.6-1.11

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+)
 
-const Button = ({handleClick, text}) => {
-  return(
-      <button onClick={handleClick}>
-        {text}
-      </button>
-  )
+const Statistics = ({good, neutral, bad}) => {
+    const all = good + neutral + bad
+    const average = (good - bad) / all
+    const positive = good / all * 100
+
+    if (all === 0){
+        return (
+            <div>
+                No feedback given
+            </div>
+        )
+    }
+    return(
+        <table>
+            <tbody>
+                <Statistic text={"good"} value={good}/>
+                <Statistic text={"neutral"} value={neutral}/>
+                <Statistic text={"bad"} value={bad}/>
+                <Statistic text={"all"} value={all}/>
+                <Statistic text={"average"} value={average}/>
+                <Statistic text={"positive"} value={positive}/>
+            </tbody>
+        </table>
+    )
 }
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0)
+const Statistic = ({text, value}) => {
+    return(
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseBuOne = () => setCounter(counter - 1)
-  const reset = () => setCounter(0)
 
-  return (
-      <div>
-        <Display counter={counter} />
-        <Button handleClick={increaseByOne} text='plus'/>
-        <Button handleClick={decreaseBuOne} text='minus'/>
-        <Button handleClick={reset} text='reset'/>
-      </div>
-  )
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+
+
+    return (
+        <div>
+            <h3>Give feedback</h3>
+            <p>
+                <Button handleClick={() => setGood(good + 1)} text={"good"}/>
+                <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"}/>
+                <Button handleClick={() => setBad(bad + 1)} text={"bad"}/>
+            </p>
+            <h3>statistics</h3>
+            <Statistics good={good} neutral={neutral} bad={bad} />
+        </div>
+    )
 }
 
 export default App
